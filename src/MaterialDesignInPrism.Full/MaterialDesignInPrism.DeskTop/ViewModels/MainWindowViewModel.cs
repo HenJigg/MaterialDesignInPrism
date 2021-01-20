40e87ec3597993a6abdbf63b5d3bfdf476e01b56
henji <zhouhaogg789@outlook.com>
2021-01-20 22:18:32 +0800

    构建首页菜单加载、日志

--- src/MaterialDesignInPrism.Full/MaterialDesignInPrism.DeskTop/ViewModels/MainWindowViewModel.cs
+++ src/MaterialDesignInPrism.Full/MaterialDesignInPrism.DeskTop/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,9 @@
-﻿using MaterialDesignInPrism.Core.Service;
-using MaterialDesignInPrism.DeskTop.Views.Dialogs;
-using MaterialDesignThemes.Wpf;
+﻿using MaterialDesignInPrism.Core.Config;
+using MaterialDesignInPrism.Core.Service;
 using Prism.Commands;
 using Prism.Mvvm;
-using System;
-using MaterialDesignInPrism.Core.Extensions;
-using System.Windows.Media;
 using Prism.Regions;
+using System.Collections.ObjectModel;
 
 namespace MaterialDesignInPrism.DeskTop.ViewModels
 {
@@ -14,21 +11,120 @@ namespace MaterialDesignInPrism.DeskTop.ViewModels
     {
         private readonly IRegionManager regionManager;
         private readonly IDialogHostService dialog;
+        private readonly IRegionNavigationJournal journal;
 
         public MainWindowViewModel(IRegionManager regionManager, IDialogHostService dialog)
         {
-            this.regionManager = regionManager;
             this.dialog = dialog;
+            this.regionManager = regionManager;
             HomeCommand = new DelegateCommand(GoHome);
+            MovePrevCommand = new DelegateCommand(MovePrev);
+            MoveNextCommand = new DelegateCommand(MoveNext);
+            NavigationCommand = new DelegateCommand<ModuleConfig>(NavigationPage);
+
+            _moduleConfigs = CreateModuleConfigs();
         }
 
+        #region Command
 
         public DelegateCommand HomeCommand { get; private set; }
+        public DelegateCommand MovePrevCommand { get; private set; }
+        public DelegateCommand MoveNextCommand { get; private set; }
+        public DelegateCommand<ModuleConfig> NavigationCommand { get; private set; }
 
+        #endregion
+
+        #region Property
+
+        private ModuleConfig _selectModuleConfig;
+
+        public ModuleConfig SelectModuleConfig
+        {
+            get { return _selectModuleConfig; }
+            set { _selectModuleConfig = value; RaisePropertyChanged(); }
+        }
+
+        #endregion
+
+
+
+        #region Method
+
+        /// <summary>
+        /// 下一步
+        /// </summary>
+        private void MoveNext()
+        {
+            if (journal != null && journal.CanGoForward)
+                journal.GoForward();
+        }
+
+        /// <summary>
+        /// 上一步
+        /// </summary>
+        private void MovePrev()
+        {
+            if (journal != null && journal.CanGoBack)
+                journal.GoBack();
+        }
+
+        /// <summary>
+        /// 返回首页
+        /// </summary>
         private void GoHome()
         {
-            regionManager.Regions["ContentRegion"].RequestNavigate("SkinView");
-            //var result = await dialog.ShowDialog("ShowView");
+            NavigationPage("HomeView");
         }
+
+        private void NavigationPage(ModuleConfig config)
+        {
+            if (config == null) return;
+
+            SelectModuleConfig = config;
+            SelectModuleConfig.IsActive = false;
+
+            NavigationPage(config.Code);
+        }
+
+        /// <summary>
+        /// 导航页
+        /// </summary>
+        /// <param name="moduleName"></param>
+        void NavigationPage(string moduleName)
+        {
+            if (string.IsNullOrWhiteSpace(moduleName)) return;
+
+            regionManager.Regions["ContentRegion"]
+                .RequestNavigate(moduleName, arg =>
+                {
+                    if (!(bool)arg.Result)
+                    {
+                        //...
+                    }
+                });
+        }
+
+        /// <summary>
+        /// 应用菜单
+        /// </summary>
+        /// <returns></returns>
+        ObservableCollection<ModuleConfig> CreateModuleConfigs()
+        {
+            return new ObservableCollection<ModuleConfig>
+            {
+                new ModuleConfig("个性化","SkinView","ColorLens")
+            };
+        }
+
+        #endregion
+
+        private ObservableCollection<ModuleConfig> _moduleConfigs;
+
+        public ObservableCollection<ModuleConfig> ModuleConfigs
+        {
+            get { return _moduleConfigs; }
+            set { _moduleConfigs = value; RaisePropertyChanged(); }
+        }
+
     }
 }
