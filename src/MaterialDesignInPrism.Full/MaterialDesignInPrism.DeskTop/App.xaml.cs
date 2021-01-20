fbda3064bf9efc5e982ae95f77eba3f5c69f4abe
henji <zhouhaogg789@outlook.com>
2021-01-20 11:23:12 +0800

    初始化wpf.Prism项目, 添加MaterialDesign组件

---
+++ src/MaterialDesignInPrism.Full/MaterialDesignInPrism.DeskTop/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MaterialDesignInPrism.DeskTop.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace MaterialDesignInPrism.DeskTop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
